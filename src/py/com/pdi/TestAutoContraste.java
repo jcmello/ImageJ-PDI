cfb9de9cec4ed21f1416d347c4ea5ae727d708cf
jmello <julio_mello@hacienda.gov.py>
2023-04-13 21:46:47 -0400

    Se implementa el algoritmo de auto-contraste

--- src/py/com/pdi/TestAutoContraste.java
+++ src/py/com/pdi/TestAutoContraste.java
@@ -1,9 +1,22 @@
 package py.com.pdi;
 
+import ij.IJ;
+import ij.ImagePlus;
+import ij.process.ImageProcessor;
+
 public class TestAutoContraste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String ruta = "C:\\Proyectos_PDI\\ImageJ-PDI\\imagenes\\einstein-low-contrast.tif";
+		ImagePlus im = IJ.openImage(ruta); 				// carga la imagen
+		im.show(); 										// Muestra la imagen original
+		ImagePlus im2 = im.duplicate(); 
+		
+		AutoContraste auto = new AutoContraste();
+		
+		ImageProcessor ip = im2.getProcessor();
+		auto.run(ip);
+		im2.show();
 
 	}
 
